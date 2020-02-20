cb73a675198180ec760bb7c3b73456d03edd1f1a
KJS4588 <wjsdnd4588@gmail.com>
2020-02-20 11:29:00 +0900

    modified

--- include/pcl_converter/converter.h
+++ include/pcl_converter/converter.h
@@ -1,18 +1,28 @@
-#include "ros/ros.h"
+#include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
+//pcl_ros
 #include <pcl_conversions/pcl_conversions.h>
-#include <pcl/point_types.h>
-#include <pcl/PCLPointCloud2.h>
-#include <pcl/conversions.h>
+#include <pcl_ros/point_cloud.h>
 #include <pcl_ros/transforms.h>
+//pcl
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+//filter
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/passthrough.h>
+//#include <visualization_msgs/Marker.h>
 
+#include <vector>
 using namespace std;
 class Converter{
 private:
     ros::NodeHandle nh_;
-    ros::Publisher pub_;
+    ros::Publisher pub_, marker_pub_;
     ros::Subscriber sub_;
-
-public:
+    
+   
+public: 
     void initSetup();
     void convertCallback(const boost::shared_ptr<const sensor_msgs::PointCloud2>& input);
     void points_Callback(const sensor_msgs::PointCloud2ConstPtr &msg);
--- src/converter.cpp
+++ src/converter.cpp
@@ -1,51 +1,106 @@
 #include "pcl_converter/converter.h"
-#include <pcl/filters/voxel_grid.h>
-#include <pcl_ros/transforms.h>
-#include <pcl/point_types.h>
-#include <pcl/io/pcd_io.h>
-#include <pcl/filters/passthrough.h>
-#include <pcl_ros/point_cloud.h>
+#include <velodyne_pointcloud/point_types.h>
+#include <visualization_msgs/Marker.h>
+#include <cmath>
 
 void Converter::convertCallback(const boost::shared_ptr<const sensor_msgs::PointCloud2>& input){
     pcl::PCLPointCloud2 pcl_pc2;
-    pcl_conversions::toPCL(*input,pcl_pc2);
-    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
-    pcl::fromPCLPointCloud2(pcl_pc2,*cloud);
-    float x = cloud -> points[0].intensity;
-    pub_.publish(cloud);
+    //pcl_conversions::toPCL(*input,pcl_pc2);
+    pcl::PointXYZI point;
+    point.x = point.y = point.z = point.intensity = 0;
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZI>);
+    pcl::PointCloud<velodyne_pointcloud::PointXYZIR>::Ptr cloud_XYZIR (new pcl::PointCloud<velodyne_pointcloud::PointXYZIR>);
+    pcl::fromROSMsg(*input, *cloud_XYZIR);
+    //fromPCLPointCloud2(pcl_pc2, *cloud_XYZIR);
+    cloud_filtered->header.frame_id = "velodyne";
+    for (size_t i = 0; i < cloud_XYZIR -> points.size(); i++){
+        if (cloud_XYZIR -> points[i].ring >= 0 && cloud_XYZIR -> points[i].ring <= 5){
+            point.x = cloud_XYZIR -> points[i].x;
+            point.y = cloud_XYZIR -> points[i].y;
+            point.z = cloud_XYZIR -> points[i].z;
+            point.intensity = cloud_XYZIR -> points[i].intensity;
+            cloud_filtered -> points.push_back(point);
+        }
+    }
+    float x = cloud_XYZIR -> points[0].ring;
+    pub_.publish(cloud_filtered);
     cout << x << endl;
     
 }
 void Converter::points_Callback(const sensor_msgs::PointCloud2ConstPtr &msg){
     pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
-    pcl::fromROSMsg(*msg, *cloud);
     pcl::VoxelGrid<pcl::PointXYZI> vg;
     pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZI>);
-    /*vg.setInputCloud(cloud);
-    vg.setLeafSize(0.1f, 0.1f, 0.1f);
-    vg.filter(*cloud_filtered);
-    cout << "after : " << cloud_filtered->points.size() << "////// before: " << cloud->points.size() << endl;*/
     pcl::PointXYZI point;
-    
-    for (std::size_t i = 0; i < cloud->points.size(); ++i){
-        if (cloud -> points[i].intensity >= 20 && cloud -> points[i].intensity <= 70){
-            point = cloud -> points[i];
-            cloud_filtered-> points.push_back(point);
+    point.x = point.y = point.z = point.intensity = 0;
+    pcl::PointXYZI point1; 
+    pcl::PointCloud<velodyne_pointcloud::PointXYZIR>::Ptr cloud_XYZIR (new pcl::PointCloud<velodyne_pointcloud::PointXYZIR>);
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered2 (new pcl::PointCloud<pcl::PointXYZI>);
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered3 (new pcl::PointCloud<pcl::PointXYZI>);
+    pcl::PointCloud<pcl::PointXYZI>::Ptr container (new pcl::PointCloud<pcl::PointXYZI>);
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered4 (new pcl::PointCloud<pcl::PointXYZI>);
+    pcl::PassThrough<pcl::PointXYZI> pass;
+    visualization_msgs::Marker mark_points;
+    mark_points.header.frame_id = "velodyne";
+    mark_points.header.stamp = ros::Time::now();
+    mark_points.ns = "mark_points";
+    mark_points.action = visualization_msgs::Marker::ADD;
+    mark_points.pose.orientation.w = 1.0;
+    mark_points.id = 0;
+    mark_points.type = visualization_msgs::Marker::POINTS;
+    mark_points.scale.x = 0.2, mark_points.scale.y = 0.2;
+    mark_points.color.g = 1.0f, mark_points.color.a = 1.0;
+    pcl::fromROSMsg(*msg, *cloud_XYZIR);
+    for (size_t i = 0; i < cloud_XYZIR->points.size(); i++){
+        if (cloud_XYZIR->points[i].ring >= 0 && cloud_XYZIR->points[i].ring <= 7 ){
+            point.x = cloud_XYZIR->points[i].x;
+            point.y = cloud_XYZIR->points[i].y;
+            point.z = cloud_XYZIR->points[i].z;
+            point.intensity = cloud_XYZIR->points[i].intensity;
+            cloud_filtered->points.push_back(point);
         }
     }
-    cloud_filtered->header.frame_id = "velodyne";
-    pub_.publish(cloud_filtered);
-    /*pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered2 (new pcl::PointCloud<pcl::PointXYZI>);
-    pcl::PassThrough<pcl::PointXYZI> pass;
-    pass.setInputCloud(cloud);
-    pass.setFilterFieldName("intensity");
-    pass.setFilterLimits(20.0, 100.0);
+    /*vg.setInputCloud(cloud_filtered);  // voxel grid//
+    vg.setLeafSize(0.1f, 0.1f, 0.1f);
+    vg.filter(*cloud_filtered2);*/
+    pass.setInputCloud(cloud_filtered);
+    pass.setFilterFieldName("y");  
+    pass.setFilterLimits(0.0, 5.0);
     pass.filter (*cloud_filtered2);
-    pub_.publish(cloud_filtered2);*/    
+    pass.setInputCloud(cloud_filtered2);
+    pass.setFilterFieldName("x");  
+    pass.setFilterLimits(0.7, 100.0);
+    pass.filter (*cloud_filtered3);
+
+    ROS_INFO("after : %d --------------> before : %d",cloud_filtered3->points.size(), cloud_filtered->points.size());
+    /*for (size_t i = 0; i < cloud_filtered2->points.size(); i++){
+        if (cloud_filtered2->points[i].intensity >= 25 && cloud_filtered2->points[i].intensity <= 75){
+            point1.x = cloud_filtered2->points[i].x;
+            point1.y = cloud_filtered2->points[i].y;
+            point1.z = cloud_filtered2->points[i].z;
+            point1.intensity = cloud_filtered2->points[i].intensity;
+            container->points.push_back(point1);    
+        }   
+    }
+    int index = container->points.size()/2;
+    geometry_msgs::Point p;
+    p.x = container->points[index].x;
+    p.y = container->points[index].y;
+    p.z = container->points[index].z;
+    mark_points.points.push_back(p);
+    marker_pub_.publish(mark_points);
+*/
+    cloud_filtered->header.frame_id = "velodyne";
+    cloud_filtered2->header.frame_id = "velodyne";
+    cloud_filtered3->header.frame_id = "velodyne";
+    cloud_filtered4->header.frame_id = "velodyne";
+    pub_.publish(cloud_filtered3);
+        
 }
 void Converter::initSetup(){
     sub_ = nh_.subscribe("velodyne_points",100,&Converter::points_Callback,this);
     pub_ = nh_.advertise<pcl::PointCloud<pcl::PointXYZI>>("filter_point", 1);
+    marker_pub_ = nh_.advertise<visualization_msgs::Marker>("visualization_marker", 10);
 }
 
 int main(int argc, char **argv){
